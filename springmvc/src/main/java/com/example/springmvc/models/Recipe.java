ac64e2a6d04063cb04154e503adea75aaf770fc9
aishwarya221 <74279677+aishwarya221@users.noreply.github.com>
2022-05-03 18:48:43 +0530

    Update Recipe.java

--- springmvc/src/main/java/com/example/springmvc/models/Recipe.java
+++ springmvc/src/main/java/com/example/springmvc/models/Recipe.java
@@ -197,3 +197,24 @@ public class Recipe {
     
     
 }
+public class User {
+
+	private String userId, password;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
